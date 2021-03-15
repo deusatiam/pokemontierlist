7864bd4081bfd7df88b0ee272847367bcf315384
Olearthur <ole_arthur@hotmail.com>
2021-03-15 20:17:58 +0100

    Added script

---
+++ website/index.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+import cgi
+
+print 'Content-type:text/html\n"
+print '<title>Vote!>/title>'
+print '<head>'
+print '<meta charset="utf-8">'
+print '<title>Vote!</title>'
+print '<link href="style.css" rel="stylesheet" type="text/css">'
+print '<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">'
+print '<link rel="icon" href="/favicon.ico" type="image/x-icon">'
+print '</head>'
+print '<body>'
+print '<ul>'
+print '<li><a href="vote.html">Vote</a></li>'
+print '<li><a href="tierlist.html">Tier List</a></li>'
+print '<li><a href="about.html">About</a></li>'
+print '</ul>'
+print '<h1> Which pokeyman is better? </h1>'
+print '</body>'
+print '</html>'
