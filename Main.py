440d807b87f19cd0ea3c5f5db2dc2a6f27abd176
deusatiam <perttu.niskanen@gmail.com>
2021-03-15 16:15:03 +0200

    test code for finding the image addresses

--- Main.py
+++ Main.py
@@ -1,1 +1,11 @@
-i
+### TEST: OPENING AN IMAGE USING AN ADDRESS FROM A PANDAS DATAFRAME
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("resources/starter9.csv")
+
+bulba = df.iloc(0)[0]
+
+print(bulba['Icon'])
+print(bulba['Sugimori'])
